49d738f9e3c7ce0c036afe95e49dadef3b8a3bfb
Rugved7 <rugvedagasti7@gmail.com>
2025-07-25 21:51:27 +0530

    Trader class added -checking out Market class

--- src/main/java/com/StockSimX/Market.java
+++ src/main/java/com/StockSimX/Market.java
@@ -1,5 +1,9 @@
 package com.StockSimX;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Market {
+    private final Lock lock = new ReentrantLock(true);
 
 }
--- src/main/java/com/StockSimX/Trader.java
+++ src/main/java/com/StockSimX/Trader.java
@@ -1,5 +1,7 @@
 package com.StockSimX;
 
+import java.util.Random;
+
 public class Trader implements Runnable {
     private final int id;
     private final Market market;
@@ -13,6 +15,20 @@ public class Trader implements Runnable {
 
     @Override
     public void run() {
+        Random random = new Random();
+        for (int i = 1; i <= ticks ; i++) {
+            try {
+                Thread.sleep(random.nextInt(500));
+                boolean buy = random.nextBoolean();
+                int quantity = random.nextInt(10) + 1;
+                int price = 100 + random.nextInt(50);
 
+                String action = buy ? "BUY" : "SELL";
+                System.out.printf("Trader %d Tick %d → %s %d shares @ ₹%d\n", id, i, action, quantity, price);
+                market.executeOrder(id,action,quantity,price);
+            }  catch (InterruptedException e) {
+                System.out.println("Trader " + id + " interrupted");
+            }
+        }
     }
 }
