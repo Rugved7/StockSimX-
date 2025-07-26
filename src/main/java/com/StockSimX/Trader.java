4d82a9a2690a9123936cc1639515a4cd3ff375e2
Rugved7 <rugvedagasti7@gmail.com>
2025-07-26 17:26:23 +0530

    Trader code started - Just basic more to implement

--- src/main/java/com/StockSimX/Trader.java
+++ src/main/java/com/StockSimX/Trader.java
@@ -1,4 +1,24 @@
 package com.StockSimX;
 
-public class Trader {
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Trader implements Runnable {
+    private final String traderId;
+    private final List<Stock> avaliableStocks;
+    private final Map<String,OrderBook> orderBooks;
+    private final Random random;
+
+    private final AtomicInteger ordersPlaced;
+    private final CyclicBarrier marketCyclicBarrier;
+    private final CountDownLatch simulationLatch;
+    private volatile boolean running;
+
+    private final int maxOrdersPerTrader;
+    private final int minTradingDelay;
+    private final int maxTradingDelay;
 }
